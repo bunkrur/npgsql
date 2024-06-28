c1b3908ac78060ae030322722546e6e0ae16d141
Nino Floris <mail@ninofloris.com>
2024-06-28 16:43:25 +0200

    Remove unused import

--- src/Npgsql/Internal/PgSerializerOptions.cs
+++ src/Npgsql/Internal/PgSerializerOptions.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
-using System.Runtime.CompilerServices;
 using System.Text;
 using Npgsql.Internal.Postgres;
 using Npgsql.NameTranslation;
